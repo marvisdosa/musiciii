f38c56e4eb4de82f26ee59d42a6a4c9f3b85fdf3
Marvis Ighedosa <dosamarvis@gmail.com>
2024-01-22 09:54:08 +0100

    Initial Commit

---
+++ musiciii/musiciiiApp.swift
@@ -0,0 +1,17 @@
+//
+//  musiciiiApp.swift
+//  musiciii
+//
+//  Created by Marvis Ighedosa on 22/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct musiciiiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
